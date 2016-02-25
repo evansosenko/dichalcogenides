e0729f116b9ecef96142b1e8f83c3f26e3fe7c48
Evan Sosenko <razorx@evansosenko.com>
2016-02-25 10:40:04 -0800

    Replace makenew boilerplate

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import find_packages, setup
 
-from makenew_python_package import __version__
+from dichalcogenides import __version__
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
-    name='makenew-python-package',
+    name='dichalcogenides',
     version=__version__,
     author='Evan Sosenko',
     author_email='razorx@evansosenko.com',
     packages=find_packages(exclude=['docs']),
-    url='https://github.com/makenew/python-package',
+    url='https://github.com/razor-x/dichalcogenides',
     license='MIT',
-    description='Python package skeleton.',
+    description='Python analysis code for dichalcogenide systems.',
     long_description=long_description,
     test_suite='nose2.collector.collector',
     install_requires=[
