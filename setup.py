327b4558d77256dd45f0e32be014960eb66734ff
Evan Sosenko <razorx@evansosenko.com>
2016-04-27 11:32:15 -0700

    Remove mocked dependencies for readthedocs

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import os
+import re
 from setuptools import find_packages, setup
 
 from dichalcogenides import __version__
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
+
+if os.environ.get('READTHEDOCS') == 'True':
+    mocked = ['numpy', 'scipy']
+    mock_filter = lambda x: re.sub(r'>.+', '', x) not in mocked
+else:
+    mock_filter = lambda p: True
 
 setup(
     name='dichalcogenides',
@@ -15,9 +23,9 @@ setup(
     license='MIT',
     description='Python analysis code for dichalcogenide systems.',
     long_description=long_description,
-    install_requires=[
+    install_requires=list(filter(mock_filter, [
         'numpy>=1.11.0,<2.0.0',
         'scipy>=0.17.0,<1.0.0',
         'pyyaml>=3.11,<4.0'
-    ]
+    ]))
 )
